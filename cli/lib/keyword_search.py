17503779c9472f38772417f831386f3301707151
Vikuuu <adivik672@gmail.com>
2025-10-11 22:32:57 +0530

    feat: Preprocessing

---
+++ cli/lib/keyword_search.py
@@ -0,0 +1,50 @@
+import string
+
+from nltk.stem import PorterStemmer
+
+from .search_utils import (
+    DEFAULT_SEARCH_LIMIT,
+    load_movies,
+    load_stopwords,
+)
+
+
+def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
+    movies = load_movies()
+    results = []
+    for movie in movies:
+        query_tokens = tokenize_text(query)
+        title_tokens = tokenize_text(movie["title"])
+
+        if has_matching_token(query_tokens, title_tokens):
+            results.append(movie)
+            if len(results) >= limit:
+                break
+    return results
+
+
+def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
+    for query_token in query_tokens:
+        for title_token in title_tokens:
+            if query_token in title_token:
+                return True
+    return False
+
+
+def preprocess_text(text: str) -> str:
+    text = text.lower()
+    translator = str.maketrans("", "", string.punctuation)
+    text = text.translate(translator)
+    return text
+
+
+def tokenize_text(text: str):
+    stopwords = load_stopwords()
+    stemmer = PorterStemmer()
+    text = preprocess_text(text)
+    tokens = text.split()
+    valid_tokens = []
+    for token in tokens:
+        if token and token not in stopwords:
+            valid_tokens.append(stemmer.stem(token))
+    return valid_tokens
