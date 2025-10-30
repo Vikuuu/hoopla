f11ac30cca428e975c0140203db78d209a42fce2
Vikuuu <adivik672@gmail.com>
2025-10-31 01:15:37 +0530

    feat: Recall value and F1 score

--- cli/evaluation_cli.py
+++ cli/evaluation_cli.py
@@ -40,9 +40,14 @@ def evaluation(limit: int):
             if res["title"] in data["relevant_docs"]:
                 relevent_retrieved += 1
         precision = relevent_retrieved / total_retrieved
+        recall = relevent_retrieved / len(data["relevant_docs"])
+        f1 = 2 * (precision * recall) / (precision + recall)
 
+        print(f"k={limit}\n")
         print(f"- Query: {data["query"]}")
         print(f"  - Precision@{limit}: {precision:.4f}")
+        print(f"  - Recall@{limit}: {recall:.4f}")
+        print(f"  - F1 Score: {f1:4f}")
         retrieved = []
         for res in results:
             retrieved.append(res["title"])
