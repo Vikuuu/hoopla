a8d84867ecc8434a3d59735dc40b02d56e941b85
Vikuuu <adivik672@gmail.com>
2025-11-10 22:52:50 +0530

    feat: Multimodal search implementation

--- cli/lib/multimodel_search.py
+++ cli/lib/multimodel_search.py
@@ -1,17 +1,61 @@
 from PIL import Image
 from sentence_transformers import SentenceTransformer
+from .semantic_search import cosine_similarity
+from .search_utils import load_movies
 
 
-class MultiModel:
-    def __init__(self, model_name="clip-ViT-B-32"):
+class MultiModal:
+    def __init__(self, documents, model_name="clip-ViT-B-32"):
         self.model = SentenceTransformer(model_name)
+        self.documents = documents
+        self.texts = []
+        for doc in documents:
+            self.texts.append(f"{doc['title']}: {doc['description']}")
+        self.text_embeddings = self.model.encode(
+            self.texts,
+            show_progress_bar=True,
+        )
 
     def embed_image(self, image_path: str):
         image_data = Image.open(image_path)
         return self.model.encode(image_data)
 
+    def search_with_image(self, image_path: str):
+        image_embedding = self.embed_image(image_path)
+        cosine_similarities = []
+        for idx, embedding in enumerate(self.text_embeddings):
+            score = cosine_similarity(
+                embedding,
+                image_embedding,
+            )
+            doc = self.documents[idx]
+            cosine_similarities.append(
+                {
+                    "id": doc["id"],
+                    "title": doc["title"],
+                    "description": doc["description"],
+                    "score": score,
+                }
+            )
+
+        return sorted(
+            cosine_similarities,
+            key=lambda x: x["score"],
+            reverse=True,
+        )[:5]
+
 
 def verify_image_embedding(image_path: str):
-    multi_model = MultiModel()
+    multi_model = MultiModal()
     embedding = multi_model.embed_image(image_path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
+
+
+def image_search_command(img_path):
+    movies = load_movies()
+    multi_model = MultiModal(movies)
+    results = multi_model.search_with_image(img_path)
+
+    for i, res in enumerate(results, 1):
+        print(f"{i}. {res['title']} (similarity: {res['score']:.3f})")
+        print(f"     {res['description'][:100]}")
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,6 +1,6 @@
 import argparse
 
-from lib.multimodel_search import verify_image_embedding
+from lib.multimodel_search import verify_image_embedding, image_search_command
 
 
 def main():
@@ -12,12 +12,20 @@ def main():
     )
     verify_image_embedding_parser.add_argument("image", type=str, help="image path")
 
+    image_search_parser = subparsers.add_parser(
+        "image_search", help="Search movie by providing the image path"
+    )
+    image_search_parser.add_argument("image", type=str, help="image path")
+
     args = parser.parse_args()
 
     match args.command:
         case "verify_image_embedding":
             image = args.image
             verify_image_embedding(image)
+        case "image_search":
+            image = args.image
+            image_search_command(image)
         case _:
             parser.print_help()
 
